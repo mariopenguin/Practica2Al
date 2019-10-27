acd9709fb023a542941ecf7400780eefabb80655
mario <mariomatamosquitos@hotmail.es>
2019-10-27 19:40:50 +0100

    Pre-Anal1

--- src/Principal.java
+++ src/Principal.java
@@ -2,20 +2,26 @@
 public class Principal {
     private static Solucion solucion = new Solucion();
 
+    /*Establecemos la llamada al algoritmo de backtracking para resolver el problema; para optimizar dicho algoritmo, le pasamos como parametro el maximo de operaciones
+    * que puede realizar y una clase auxiliar solucion.
+    *  */
     public static int numMinMutaciones(String cadena1, String cadena2) {
         if (cadena1.length()>cadena2.length()){
-            algoritmoAux1(cadena1,cadena2, 0, 0, false,solucion,cadena1.length());
+            backtracking(cadena1,cadena2, 0, 0, false,solucion,cadena1.length());
         }else if(cadena2.length()>30){
-            algoritmoAux1(cadena1,cadena2, 0, 0, false,solucion,16);
+            backtracking(cadena1,cadena2, 0, 0, false,solucion,16);
         }
         else{
-            algoritmoAux1(cadena1,cadena2, 0, 0, false,solucion,cadena2.length());
+            backtracking(cadena1,cadena2, 0, 0, false,solucion,cadena2.length());
         }
         int aux = solucion.getNumMovimientos();
         solucion = new Solucion();
         return aux;
     }
-
+    
+    /*Este metodo se encarga de realizar las operaciones sobre la string destino en funcion del codigo de operacion y la posicion sobre la que se desea
+    * realizar dicho cambio */
+ 
     private static String operar(int operador, String cadena1, String cadena2, int posicion) {
         StringBuilder resultado = new StringBuilder(cadena1);
         if (operador == 0) {
@@ -33,13 +39,12 @@ public class Principal {
                 solucion.incrementarAvanzar();
                 posicion++;
             }
-
-        }else{
-
         }
         return resultado.toString();
     }
-
+    /*Metodo cuya finalidad es conocer  si se puede llevar a cabo una operacion, todas aquellas operaciones 
+    no permitidas devuelven un false y no se realizan*/
+    
     private static boolean aceptable(int operador, String cadena, String cadena2, int posicion) {
         if (operador == 0 && cadena.length() > posicion && cadena2.length() > posicion && cadena.charAt(posicion)!= cadena2.charAt(posicion)) {
             return true;
@@ -55,8 +60,8 @@ public class Principal {
         }
     }
 
-
-    private static void algoritmoAux1(String s1, String s2, int posicion, int numMovimientos, boolean exito,Solucion solucion,int maxMov) {
+    /*Algoritmo de backtracking principal */
+    private static void backtracking(String s1, String s2, int posicion, int numMovimientos, boolean exito, Solucion solucion, int maxMov) {
         String aux = "";
         int operacion = 0;
         do {
@@ -74,15 +79,15 @@ public class Principal {
                 aux= operar(operacion,s1,s2,posicion);
                 numMovimientos = numMovimientos+1;
                 if (operacion==1 || operacion ==2){
-                    algoritmoAux1(aux,s2,posicion,numMovimientos,exito,solucion,maxMov);//Desanotar
+                    backtracking(aux,s2,posicion,numMovimientos,exito,solucion,maxMov);//Desanotar
                 }else if(operacion == 0){
-                    algoritmoAux1(aux,s2,posicion+1,numMovimientos,exito,solucion,maxMov);//Desanotar
+                    backtracking(aux,s2,posicion+1,numMovimientos,exito,solucion,maxMov);//Desanotar
                 }
                 else{
                     numMovimientos = numMovimientos-1;
                     int aux1 = solucion.getAvanzar();
                     solucion.resetearAvanzar();
-                    algoritmoAux1(aux,s2,posicion+aux1,numMovimientos,exito,solucion,maxMov);//Desanotar
+                    backtracking(aux,s2,posicion+aux1,numMovimientos,exito,solucion,maxMov);//Desanotar
                 }
                 if (operacion!=3) numMovimientos--;
             }operacion++;
